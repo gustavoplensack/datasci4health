9beb682dd7ac1034dccf958312305904f06f3221
Gustavo <gustavoplensack@gmail.com>
2021-04-08 15:57:35 -0300

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Projeto final IA368X',
+    author='gustavoplensack',
+    license='MIT',
+)
