d5fd798b8911f1cbe8bfac697128cd354168b8a4
Gustavo <gustavoplensack@gmail.com>
2021-06-15 17:25:40 -0300

    Add generated images path

--- src/visualization/caged/caged_by_region.py
+++ src/visualization/caged/caged_by_region.py
@@ -11,6 +11,7 @@ CAGED_SERIES_OPTIONS = ['Saldos', 'Estoque', 'Admissões', 'Desligamentos']
 
 def visualize_caged_by_region(
     caged_with_region_path: str,
+    output_path: str,
     region: Optional[str] = None,
     series: str = 'Estoque'
 ):
@@ -33,8 +34,9 @@ def visualize_caged_by_region(
         raise KeyError(f'Series {series} is not a valid option! '
                        f'Valid options are {CAGED_SERIES_OPTIONS}')
 
-    # Filter only selected series
-    caged_selected_series = caged_with_region_df.filter(regex=(f"{series}."))
+    # Drop rows without values for 'Valor de Display Classificação'
+    caged_with_region_df.dropna(subset=['Valor de Display Classificação'],
+                                inplace=True)
 
     # Filter by region if specified
     if region is not None:
@@ -42,8 +44,30 @@ def visualize_caged_by_region(
             caged_with_region_df['Valor de Display Classificação'] == region
         ].copy()
 
+    # Aggregate using SUM
+    caged_with_region_df = caged_with_region_df.groupby(
+        by=['Valor de Display Classificação']).sum()
+
+    # Filter only selected series and 'Valor de Display Classificação'
+    caged_selected_series = caged_with_region_df.filter(regex=(f"{series}."))
+
+    # Remove series name from the dataframe
+    columns_map = {key: key.replace(series, '')
+                   for key in list(caged_selected_series.columns)}
+    caged_selected_series = caged_selected_series.rename(columns=columns_map)
+
+    # Traspose to get a timeseries-like
     caged_selected_series = caged_selected_series.transpose(copy=True)
 
+    # Plot an image, add grid and rotate ticks for better fitting
     plot = caged_selected_series.plot()
+    plot.grid(True)
+    plot.tick_params(rotation=15)
 
-    plot.get_figure().savefig('output.png')
+    # Set image title and filename
+    if region is None:
+        plot.set_title(f'{series}-CAGED')
+        plot.get_figure().savefig(f'{output_path}/caged-{series}.png')
+    else:
+        plot.set_title(f'{series}-CAGED@{region}')
+        plot.get_figure().savefig(f'{output_path}/caged-{region}-{series}.png')
